fefdb16969fc96ddce901c23a3dc99970844e02e
RAKSHANA R <rakshanarajagopal93@gmail.com>
2026-03-08 12:58:43 +0530

    Enhance form validation and submission feedback
    Added email validation and improved form submission logic.

--- form.js
+++ form.js
@@ -1,24 +1,30 @@
-var username=document.getElementById("username")
-var pass=document.getElementById("pass")
-var cpass=document.getElementById("cpass")
-var btn=document.getElementById("btn")
+var username = document.getElementById("username")
+var mail = document.getElementById("mail")
+var pass = document.getElementById("pass")
+var cpass = document.getElementById("cpass")
+var btn = document.getElementById("btn")
 
-btn.addEventListener("click",function(e){
-    e.preventDefault() 
-    if(pass.value == "" && cpass.value==""){
-        alert("Please enter the password")
+btn.addEventListener("click", function (e) {
+    e.preventDefault()
+    if (pass.value == "" || cpass.value == "" || username.value == "" || !mail.value.include("@")) {
+        alert("Please fill valid details")
     }
-    if(pass.value===cpass.value){
+    else if (pass.value === cpass.value && pass.value !== "" && cpass.value !== "") {
         var oldMsg = document.querySelector(".success")
-        if(oldMsg){
+        if (oldMsg) {
             oldMsg.remove()
         }
-   var submit=document.createElement("h3")
-   submit.className = "success"
+        var submit = document.createElement("h3")
+        submit.className = "success"
 
-    submit.textContent="Hey " + username.value + ",  Your Form Was Submitted Sucessfully🎉✅"
-    document.querySelector(".form").appendChild(submit)}
-    {
+        submit.textContent = "Hey " + username.value + ",  Your Form Was Submitted Sucessfully🎉✅"
+        document.querySelector(".form").appendChild(submit)
+        
+        setTimeout(function(){
+            location.reload()
+        },2000)
+    }
+    else {
         alert("Password Doesn't Match, Please Confirm the password❌")
     }
 })
