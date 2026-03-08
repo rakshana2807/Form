caec45dda4106f03c52e10d2a36863d2815c4d98
RAKSHANA R <rakshanarajagopal93@gmail.com>
2026-03-08 13:04:25 +0530

    Refactor form validation logic and messages
    Updated form validation to include additional checks for email format and improved alert messages.

--- form.js
+++ form.js
@@ -5,26 +5,32 @@ var cpass = document.getElementById("cpass")
 var btn = document.getElementById("btn")
 
 btn.addEventListener("click", function (e) {
+
     e.preventDefault()
-    if (pass.value == "" || cpass.value == "" || username.value == "" || !mail.value.include("@")) {
-        alert("Please fill valid details")
+
+    if (username.value === "" || mail.value === "" || pass.value === "" || cpass.value === "" || !mail.value.includes("@") || !mail.value.includes(".")) {
+        alert("Please enter valid details")
     }
-    else if (pass.value === cpass.value && pass.value !== "" && cpass.value !== "") {
+
+    else if (pass.value === cpass.value) {
+
         var oldMsg = document.querySelector(".success")
         if (oldMsg) {
             oldMsg.remove()
         }
+
         var submit = document.createElement("h3")
         submit.className = "success"
+        submit.textContent = "Hey " + username.value + ", Your Form Was Submitted Successfully 🎉✅"
 
-        submit.textContent = "Hey " + username.value + ",  Your Form Was Submitted Sucessfully🎉✅"
         document.querySelector(".form").appendChild(submit)
-        
-        setTimeout(function(){
+
+        setTimeout(function () {
             location.reload()
-        },2000)
+        }, 2000)
     }
+
     else {
-        alert("Password Doesn't Match, Please Confirm the password❌")
+        alert("Password Doesn't Match, Please Confirm the password ❌")
     }
 })
