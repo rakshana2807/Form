0bc55ceb15d3751a9d2e9d3b9715f8561fe538ad
RAKSHANA R <rakshanarajagopal93@gmail.com>
2026-03-08 12:33:27 +0530

    Add password validation and alert for empty fields

--- form.js
+++ form.js
@@ -1,22 +1,24 @@
-var username = document.getElementById("username")
-var pass = document.getElementById("pass")
-var cpass = document.getElementById("cpass")
-var btn = document.getElementById("btn")
+var username=document.getElementById("username")
+var pass=document.getElementById("pass")
+var cpass=document.getElementById("cpass")
+var btn=document.getElementById("btn")
 
-btn.addEventListener("click", function (e) {
-    e.preventDefault()
-    if (pass.value === cpass.value) {
+btn.addEventListener("click",function(e){
+    e.preventDefault() 
+    if(pass.value == "" && cpass.value==""){
+        alert("Please enter the password")
+    }
+    if(pass.value===cpass.value){
         var oldMsg = document.querySelector(".success")
-        if (oldMsg) {
+        if(oldMsg){
             oldMsg.remove()
         }
-        var submit = document.createElement("h3")
-        submit.className = "success"
+   var submit=document.createElement("h3")
+   submit.className = "success"
 
-        submit.textContent = "Hey " + username.value + ",  Your Form Was Submitted Sucessfully🎉✅"
-        document.querySelector(".form").appendChild(submit)
-    }
-    else {
+    submit.textContent="Hey " + username.value + ",  Your Form Was Submitted Sucessfully🎉✅"
+    document.querySelector(".form").appendChild(submit)}
+    {
         alert("Password Doesn't Match, Please Confirm the password❌")
     }
 })
